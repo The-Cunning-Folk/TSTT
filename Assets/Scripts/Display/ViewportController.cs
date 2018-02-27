8123d926d8496f91a332adc048b73d806f91108c
forsterjoel@gmail.com <forsterjoel@gmail.com>
2018-02-27 22:58:35 +0000

    initial commit

---
+++ Assets/Scripts/Display/ViewportController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewportController : MonoBehaviour {
+
+	private Camera dialogCam;
+	private Camera gameCam;
+	static float t = 0.0f;
+
+	private float gameCamXSize = 0.5f;
+
+	private float min = 0.5F;
+	private float max =  0.5F;
+
+	public float rate = 0.5f;
+
+	private Transform target;
+
+
+	void Start()
+	{
+		dialogCam = transform.GetChild(0).GetComponent<Camera>();
+		gameCam = transform.GetChild(1).GetComponent<Camera>();
+		target = GameObject.Find ("Character").transform;
+	}
+
+	void ChangeGameCamSize(float newSize){
+		min = gameCamXSize;
+		max = newSize;
+		t = 0.0f;
+	}
+
+	void Update()
+	{
+		if (Input.GetKey ("space")) {
+			ChangeGameCamSize (Random.Range (0.0f, 1.0f));
+		}
+
+		gameCamXSize = Mathf.Lerp (min, max, t);
+
+		t += rate * Time.deltaTime;
+
+		// setup the rectangle
+		dialogCam.rect = new Rect (gameCamXSize, 0.0f, 1.0f - gameCamXSize, 1.0f);
+		gameCam.rect = new Rect (0.0f, 0.0f, gameCamXSize, 1.0f);
+
+		gameCam.transform.position = target.position - new Vector3(0.0f,0.0f,5.0f);
+
+	}
+}
---
+++ Assets/Scripts/Input/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class UserInput : MonoBehaviour {
+
+	public float speed=0.1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float hRaw = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+		float vRaw = CrossPlatformInputManager.GetAxisRaw("Vertical");
+		if (Mathf.Abs (hRaw) > 0 || Mathf.Abs (vRaw) > 0) {
+			float rInputMag = 1.0f / Mathf.Sqrt (hRaw * hRaw + vRaw * vRaw);
+			Vector3 inputUnit = new Vector3 (hRaw * rInputMag, vRaw * rInputMag, 0);
+			transform.Translate (speed*inputUnit);
+		}
+	}
+}

a0289eb47e577acd341101b9faf3a7e862b87957
forsterjoel@gmail.com <forsterjoel@gmail.com>
2018-03-07 23:21:58 +0000

    added triggers and tiles

---
+++ Assets/Scripts/GameLogic/Player/PlayerBehaviours.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviours : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
